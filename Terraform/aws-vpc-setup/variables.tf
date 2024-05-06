a883a736882897efd9c37b8e35aa25b30fb471c4
adayava1sourav <souravsb4498@gmail.com>
2024-05-06 13:19:49 +0530

    draft2

--- Terraform/aws-vpc-setup/variables.tf
+++ Terraform/aws-vpc-setup/variables.tf
@@ -1,31 +1,31 @@
 variable "project" {
     type = string
-    default = "myproject"
+    default = "myproject"  // Add your project name
   
 }
 
 variable "env" {
     type = string
-    default = "dev"
+    default = "dev"  // Add your environmnet name
   
 }
 
 variable "vpc1_cidr" {
     type = string
-    default = "10.0.0.0/24"
+    default = "10.0.0.0/24" // Add your VPC CIDR range
 }
 
 
 
 variable "pub_sub_1_cidr" {
     type = string
-    default = "10.0.0.0/26"
+    default = "10.0.0.0/26" // Add your public subnet 1 CIDR range
   
 }
 
 variable "pub_sub_2_cidr" {
     type = string
-    default = "10.0.0.64/26"
+    default = "10.0.0.64/26" // Add your public subnet 2 CIDR range
   
 }
 
@@ -35,13 +35,13 @@ variable "pub_sub_2_cidr" {
 
 variable "priv_sub_1_cidr" {
     type = string
-    default = "10.0.0.128/26"
+    default = "10.0.0.128/26" // Add your private subnet 2 CIDR range
   
 }
 
 variable "priv_sub_2_cidr" {
     type = string
-    default = "10.0.0.192/26"
+    default = "10.0.0.192/26" // Add your private subnet 2 CIDR range
   
 }
 
